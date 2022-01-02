40f225f28a2047f518fd9d7a7f0fa1ea53dbdd10
Braxiatel <kheliz404@gmail.com>
2022-01-02 23:12:53 +0300

    - fix test with regard to changes in log_analyzer.py

--- tests/test_log_analyzer.py
+++ tests/test_log_analyzer.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import log_analyzer
+from datetime import datetime
 
 test_config = {
     "REPORT_SIZE": 1000,
@@ -14,19 +15,19 @@ class TestLogLocation(unittest.TestCase):
 
     def test_latest_gz_file(self):
         found_file = log_analyzer.find_latest_log(test_config.get('LOG_DIR'))
-        self.assertEqual(found_file.filename, 'nginx-access-ui.log-20160631.gz')
+        self.assertEqual(found_file.filename, 'nginx-access-ui.log-20160630.gz')
 
     def test_bz_log_file(self):
         found_file = log_analyzer.find_latest_log(test_config.get('LOG_DIR'))
-        self.assertNotEqual(found_file.filename, 'nginx-access-ui.log-20180631.bz')
+        self.assertNotEqual(found_file.filename, 'nginx-access-ui.log-20180630.bz')
 
     def test_invalid_log_file(self):
         found_file = log_analyzer.find_latest_log(test_config.get('LOG_DIR'))
-        self.assertNotEqual(found_file.filename, 'packet_capture.log-20180631.gz')
+        self.assertNotEqual(found_file.filename, 'packet_capture.log-20180630.gz')
 
     def test_plain_log_file(self):
         found_file = log_analyzer.find_latest_log(os.path.join(test_config.get('LOG_DIR'), "./plain_dir"))
-        self.assertEqual(found_file.filename, 'nginx-access-ui.log-20160631')
+        self.assertEqual(found_file.filename, 'nginx-access-ui.log-20160630')
 
     def check_empty_log_directory(self):
         with self.assertRaises(RuntimeError):
@@ -37,28 +38,34 @@ class TestLogLocation(unittest.TestCase):
 class TestLogAnalyzing(unittest.TestCase):
 
     def test_file_analyzed(self):
-        analyzed_file = log_analyzer.is_file_analyzed(20160631, config=test_config)
+        format_data = "%Y%m%d"
+        new_date = datetime.strptime('20160630', format_data)
+        analyzed_file = log_analyzer.is_file_analyzed(new_date, config=test_config)
         self.assertEqual(analyzed_file, True)
 
     def test_empty_log(self):
-        self.assertEqual(log_analyzer.handle_file(log_file="nginx-access-ui.log-20160631.gz", config=test_config), None)
+        self.assertEqual(log_analyzer.handle_file(log_analyzer.read_file_into_data,
+                                                  log_file="nginx-access-ui.log-20160630.gz",
+                                                  config=test_config),
+                         None)
 
     def test_many_errors(self):
-        test_file = os.path.join('plain_dir', 'nginx-access-ui.log-20160631')
-        with self.assertRaises(RuntimeError):
-            test = log_analyzer.handle_file(log_file=test_file, config=test_config)
+        test_file = os.path.join('plain_dir', 'nginx-access-ui.log-20160630')
+        with self.assertRaises(ValueError):
+            test = log_analyzer.handle_file(log_analyzer.read_file_into_data, log_file=test_file, config=test_config)
 
     def test_result_generator(self):
         new_config = test_config.copy()
         new_config['LOG_DIR'] = os.path.join(test_config.get("LOG_DIR"), "valid_dir")
-        test = log_analyzer.handle_file("nginx-access-ui.log-20180631", config=new_config)
+        test = log_analyzer.handle_file(log_analyzer.read_file_into_data,
+                                        "nginx-access-ui.log-20180630", config=new_config)
         self.assertEqual(sum(item['count'] for item in test), 10)
 
 
 class TestConfiguration(unittest.TestCase):
 
     def test_missing_configuration_file(self):
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(ValueError):
             log_analyzer.main("nonexistent.config")
 
     def test_config_changed(self):
@@ -68,12 +75,12 @@ class TestConfiguration(unittest.TestCase):
 
     def test_invalid_config(self):
         config_file = os.path.join(test_config.get("TEST_DIR"), "test_invalid_config.json")
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(OSError):
             result = log_analyzer.check_configuration(configuration_file=config_file)
 
 
 class TestReportCreation(unittest.TestCase):
-    report = os.path.join(test_config.get("REPORT_DIR"), "report-2018.06.31.html")
+    report = os.path.join(test_config.get("REPORT_DIR"), "report-2018.06.30.html")
     default_report = os.path.join(test_config.get("REPORT_DIR"), "report.html")
 
     def setUp(self):
@@ -87,7 +94,7 @@ class TestReportCreation(unittest.TestCase):
         self.assertGreater(os.path.getsize(self.report), os.path.getsize(self.default_report))
 
     def tearDown(self):
-        report = os.path.join(test_config.get("REPORT_DIR"), "report-2018.06.31.html")
+        report = os.path.join(test_config.get("REPORT_DIR"), "report-2018.06.30.html")
         os.remove(report)
 
 
